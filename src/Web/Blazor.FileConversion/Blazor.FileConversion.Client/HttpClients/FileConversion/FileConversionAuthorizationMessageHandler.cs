a2482773aed0a23b32b84a1a812bcc0a00095c8c
TRAN HUU DUC <thd1152015@gmail.com>
2021-03-03 22:22:07 +0700

    PWA

--- src/Web/Blazor.FileConversion/Blazor.FileConversion.Client/HttpClients/FileConversion/FileConversionAuthorizationMessageHandler.cs
+++ src/Web/Blazor.FileConversion/Blazor.FileConversion.Client/HttpClients/FileConversion/FileConversionAuthorizationMessageHandler.cs
@@ -6,13 +6,13 @@ namespace Blazor.FileConversion.Client.HttpClients.FileConversion
 {
     public class FileConversionAuthorizationMessageHandler : AuthorizationMessageHandler
     {
-        private readonly FileConversionSettings _settings;
-
         public FileConversionAuthorizationMessageHandler(IAccessTokenProvider provider, NavigationManager navigation,
             IConfiguration configuration) :
             base(provider, navigation)
         {
-            ConfigureHandler(authorizedUrls: new[] {configuration[$"{FileConversionHttpClient.FileConversionHttpClientSettings}:BaseAddress"]},
+            ConfigureHandler(
+                authorizedUrls: new[]
+                    {configuration[$"{FileConversionHttpClient.FileConversionHttpClientSettings}:BaseAddress"]},
                 scopes: new[] {"services.fileconversion.parse"});
         }
     }
